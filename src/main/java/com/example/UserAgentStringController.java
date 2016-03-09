b65e0223506d40ff2259fff1eccdebf2e1dc72d7
skliew <skliew@gmail.com>
2016-03-09 22:41:39 +0800

    First commit

---
+++ src/main/java/com/example/UserAgentStringController.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import eu.bitwalker.useragentutils.UserAgent;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UserAgentStringController {
+
+    @RequestMapping(path="/uas")
+    public UserAgent uas(@RequestParam(value="uas", defaultValue="") final String uas, @RequestHeader("User-Agent") final String headerUas) {
+        String userAgentString = uas.isEmpty() ? headerUas : uas;
+        UserAgent userAgent = new UserAgent(userAgentString);
+        return userAgent;
+    }
+}
