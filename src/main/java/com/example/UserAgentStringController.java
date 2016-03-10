f17d36cd77c0b2ce20cb4a1d063b6693e10a8d6a
skliew <skliew@gmail.com>
2016-03-10 12:04:21 +0800

    Change path to /ua

--- src/main/java/com/example/UserAgentStringController.java
+++ src/main/java/com/example/UserAgentStringController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class UserAgentStringController {
 
-    @RequestMapping(path="/uas")
+    @RequestMapping(path="/ua")
     public UserAgent uas(@RequestParam(value="uas", defaultValue="") final String uas, @RequestHeader("User-Agent") final String headerUas) {
         String userAgentString = uas.isEmpty() ? headerUas : uas;
         UserAgent userAgent = new UserAgent(userAgentString);
