3d9a70920aa593d2772a71bce5943c07a58073f4
skliew <skliew@gmail.com>
2016-03-19 00:24:33 +0800

    Rename controller

--- src/main/java/com/example/UtilsController.java
+++ src/main/java/com/example/UtilsController.java
@@ -1,16 +1,13 @@
 package com.example;
 
-import com.maxmind.geoip2.exception.GeoIp2Exception;
-import com.maxmind.geoip2.model.CityResponse;
 import eu.bitwalker.useragentutils.UserAgent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
 
 @RestController
-public class UserAgentStringController {
+public class UtilsController {
 
     @Autowired
     GeoLocationService geoLocationService;
